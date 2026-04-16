374bf8b83a27dcdf369573e26d1f92a544c01447
sromerop <sromerop@iesrafaelalberti.es>
2026-04-16 12:05:49 +0200

    Cambiamos la clase ReservaRepository.kt para que delegue en el DAO

--- src/main/kotlin/es/iesra/datos/ReservaRepository.kt
+++ src/main/kotlin/es/iesra/datos/ReservaRepository.kt
@@ -2,21 +2,14 @@ package es.iesra.datos
 
 import es.iesra.dominio.Reserva
 
-/**
- * Implementación en memoria del repositorio de reservas.
- */
-class ReservaRepository : IReservaRepository {
-    private val reservas = mutableListOf<Reserva>()
+class ReservaRepository(private val dao: IReservaDAO) : IReservaRepository {
 
     override fun agregar(reserva: Reserva): Boolean {
-        var agregado = false
-        // Si no existe, se agrega la reserva a la lista.
-        if (!reservas.contains(reserva)) {
-            reservas.add(reserva)
-            agregado = true
-        }
-        return agregado
+        dao.guardar(reserva)
+        return true
     }
 
-    override fun obtenerTodas(): List<Reserva> = reservas.toList()
+    override fun obtenerTodas(): List<Reserva> {
+        return dao.obtenerTodas()
+    }
 }
